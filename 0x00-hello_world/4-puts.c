ce498dbff11775514235d301f525bab46947e4ff
saviour-22 <akpobisaviour200@gmail.com>
2023-02-19 05:40:39 -0800

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
-#include <studio.h>
-
-/** 
- * main - this is the main function
- *
- * Return: always 0
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
  */
-
 int main(void)
 {
-	puts(*\"Programming is like building a multilingual puzzle"):
-		return(o):
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
